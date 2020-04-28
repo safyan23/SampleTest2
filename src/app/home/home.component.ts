c68c248bd1ea8289eec8f7262564dcb38e19c24a
Safyan Iqbal <safyan23@Safyans-MacBook-Pro.local>
2020-04-27 21:38:06 -0700

    finished sample exam

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,12 @@ export class HomeComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    // this.activatedRoute.params.subscribe((sss) => {
+    //   this.data = sss;
+    //   console.log('this.data from home.....', this.data);
+    // });
 
+    this.data = JSON.parse(localStorage.getItem('calculatedData'));
   }
 
 }
